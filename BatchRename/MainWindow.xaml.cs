a372eb69d1a24feeb2bc00987f2f527fc04c060a
Nguyen Duc Hieu <hieu.nguyen@southasiaservices.com>
2022-10-30 10:06:19 +0700

    add file & folder

--- BatchRename/MainWindow.xaml.cs
+++ BatchRename/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using Fluent;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,11 +10,13 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Panel = System.Windows.Controls.Panel;
 
 namespace BatchRename
 {
@@ -24,6 +28,48 @@ namespace BatchRename
         public MainWindow()
         {
             InitializeComponent();
+            Panel.SetZIndex(FileCanvas, 1);
+            Panel.SetZIndex(FolderCanvas, 0);
+        }
+        private void addFile_Click(object sender, RoutedEventArgs e)
+        {
+            Panel.SetZIndex(FileCanvas, 1);
+            Panel.SetZIndex(FolderCanvas, 0);
+            var screen = new Microsoft.Win32.OpenFileDialog();
+            screen.Multiselect = true;
+            if (screen.ShowDialog() == true)
+            {
+                foreach (var file in screen.FileNames)
+                {
+                    FileTab.Items.Add(new File()
+                    {
+                        Filename = System.IO.Path.GetFileName(file),
+                        Path = file
+                    });
+                }
+            }
+        }
+
+        private void addFolder_Click(object sender, RoutedEventArgs e)
+        {
+            Panel.SetZIndex(FolderCanvas, 1);
+            Panel.SetZIndex(FileCanvas, 0);
+            string directory;
+            var screen = new FolderBrowserDialog();
+            if (screen.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                directory = screen.SelectedPath;
+                string[] subDirectory = Directory.GetDirectories(directory);
+
+                foreach (var dir in subDirectory)
+                {
+                    FolderTab.Items.Add(new Folder()
+                    {
+                        Foldername = dir.Substring(directory.Length + 1),
+                        Path = dir
+                    });
+                }
+            }
         }
     }
 }
--- BatchRename/obj/Debug/net6.0-windows/App.g.i.cs
+++ BatchRename/obj/Debug/net6.0-windows/App.g.i.cs
@@ -48,7 +48,7 @@ namespace BatchRename {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.8.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.9.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
@@ -60,7 +60,7 @@ namespace BatchRename {
             
             #line default
             #line hidden
-            System.Uri resourceLocater = new System.Uri("/BatchRename;V1.0.0.0;component/app.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/BatchRename;component/app.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\App.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
@@ -74,7 +74,7 @@ namespace BatchRename {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.8.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "6.0.9.0")]
         public static void Main() {
             BatchRename.App app = new BatchRename.App();
             app.InitializeComponent();
